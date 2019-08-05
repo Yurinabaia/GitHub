8fc405efde38a567110699ba6c6054e362f12076
Yuri nb <yurinabaiaduarte@gmail.com>
2019-08-05 11:44:57 -0300

    Projeto iniciado fazendo varias coisas

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp147
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for(int i = 10; i > -1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
